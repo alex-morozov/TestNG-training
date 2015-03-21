a65ab029559eeb93b35d93a7dff9549e29ee3cde
Bassman1988 <89032735516@yandex.ru>
2015-03-21 13:41:52 +0300

    5 задание

--- src/test/java/ru/trainings/common/TestBase.java
+++ src/test/java/ru/trainings/common/TestBase.java
@@ -9,6 +9,7 @@ import org.testng.annotations.BeforeTest;
 
 public class TestBase {
 	public static WebDriver driver = new FirefoxDriver();
+	public static SeleniumAssertion check = new SeleniumAssertion (driver);		
 	public static String baseURL = "http://www.musicforums.ru/";
 		
 	@BeforeTest (alwaysRun = true)
--- src/test/java/ru/trainings/tests/TestClass.java
+++ src/test/java/ru/trainings/tests/TestClass.java
@@ -6,35 +6,39 @@ import static org.testng.AssertJUnit.assertTrue;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.trainings.common.SeleniumAssertion;
 import ru.trainings.common.TestBase;
+import org.openqa.selenium.By;
 
 
 public class TestClass extends TestBase{	 
 		 	  
 	@Test ( groups = ("positive"))
 	public static void LoginOK (){
-		    TestBase.driver.findElement(By.linkText("Войти")).click();
-		    TestBase.driver.findElement(By.name("loginuser")).clear();
-		    TestBase.driver.findElement(By.name("loginuser")).sendKeys("SeleniumBass");
-		    TestBase.driver.findElement(By.name("loginpassword")).clear();
-		    TestBase.driver.findElement(By.name("loginpassword")).sendKeys("elena1989");
-		    TestBase.driver.findElement(By.name("btnlogin")).click();		   
+		    driver.findElement(By.linkText("Войти")).click();
+		    driver.findElement(By.name("loginuser")).clear();
+		    driver.findElement(By.name("loginuser")).sendKeys("SeleniumBass");
+		    driver.findElement(By.name("loginpassword")).clear();
+		    driver.findElement(By.name("loginpassword")).sendKeys("elena1989");
+		    driver.findElement(By.name("btnlogin")).click();		    
+		    check.assertDisplayed(driver.findElement(By.cssSelector("div.user-name")));		    
 	}
 	
 	@Test ( groups = ("negative"))
 	public static void LoginWrong (){
-		    TestBase.driver.findElement(By.linkText("Войти")).click();
-		    TestBase.driver.findElement(By.name("loginuser")).clear();
-		    TestBase.driver.findElement(By.name("loginuser")).sendKeys("Wrong");
-		    TestBase.driver.findElement(By.name("loginpassword")).clear();
-		    TestBase.driver.findElement(By.name("loginpassword")).sendKeys("elena1989");
-		    TestBase.driver.findElement(By.name("btnlogin")).click();		   
+		    driver.findElement(By.linkText("Войти")).click();
+		    driver.findElement(By.name("loginuser")).clear();
+		    driver.findElement(By.name("loginuser")).sendKeys("Wrong");
+		    driver.findElement(By.name("loginpassword")).clear();
+		    driver.findElement(By.name("loginpassword")).sendKeys("elena1989");
+		    driver.findElement(By.name("btnlogin")).click();	
+		    check.assertDisplayed(driver.findElement(By.name("btnlogin")));
 	}
 	
 	@Test ( groups = ("positive"))
 	public static void showButton (){
-		TestBase.driver.findElement(By.linkText("бас-гитары")).click();
-		TestBase.driver.findElement(By.cssSelector("input[type=\"button\"]"));		
+		    driver.findElement(By.linkText("бас-гитары")).click();
+		    check.assertDisplayed(driver.findElement(By.cssSelector("input[type=\"button\"]")));			    
 	}
 	
 }
